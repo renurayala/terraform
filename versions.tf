1a2d6c931ed86fb2e52a860b4051a2d4bd9ce38a
renurayala <89176932+renurayala@users.noreply.github.com>
2021-11-23 11:46:38 +0530

    Add files via upload

---
+++ versions.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-2"
+  access_key = "AKIAT3XDJKZHTTAO5P45"
+  secret_key = "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"
+}
